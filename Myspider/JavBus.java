f633d021555d749f523a131659f3c04f24696f7d
crarm <61692711+crarm@users.noreply.github.com>
2025-12-04 12:35:38 +0800

    Update and rename JavBus.java00 to JavBus.java

--- Myspider/JavBus.java
+++ Myspider/JavBus.java
@@ -16,12 +16,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Jable extends Spider {
 
     private static final String siteUrl = "https://javbus.sbs";
-    private static final String cateUrl = siteUrl + "/categories/";
-    private static final String detailUrl = siteUrl + "/videos/";
+    private static final String cateUrl = siteUrl + "/vod/type/id/";
+    private static final String detailUrl = siteUrl + "vod/detail/id/";
     private static final String searchUrl = siteUrl + "/search/";
 
     private HashMap<String, String> getHeaders() {
@@ -35,10 +37,11 @@ public class Jable extends Spider {
         List<Vod> list = new ArrayList<>();
         List<Class> classes = new ArrayList<>();
         Document doc = Jsoup.parse(OkHttp.string(cateUrl, getHeaders()));
-        for (Element element : doc.select("div.img-box")) {
-            String typeId = element.select("a").attr("href").split("/")[4];
-            String typeName = element.select("div.absolute-center > h4").text();
-            classes.add(new Class(typeId, typeName));
+        Document doc = Jsoup.parse(OkHttp.string(siteUrl, getHeaders()));
+        String[] typeIdList = {"1","2","3","4","5","6","7","all"};
+        String[] typeNameList = {"中字一区","中字二区","中字三区","中字四区","中字五区","传媒映画","日本AV","全部"};
+        for (int i = 0; i < typeNameList.length; i++) {
+            classes.add(new Class(typeIdList[i], typeNameList[i]));
         }
         doc = Jsoup.parse(OkHttp.string(siteUrl, getHeaders()));
         for (Element element : doc.select("div.video-img-box")) {
@@ -55,32 +58,52 @@ public class Jable extends Spider {
     @Override
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) throws Exception {
         List<Vod> list = new ArrayList<>();
-        String target = cateUrl + tid + "/?mode=async&function=get_block&block_id=list_videos_common_videos_list&sort_by=post_date&from=" + String.format(Locale.getDefault(), "%02d", Integer.parseInt(pg)) + "&_=" + System.currentTimeMillis();
-        Document doc = Jsoup.parse(OkHttp.string(target, getHeaders()));
-        for (Element element : doc.select("div.video-img-box")) {
-            String pic = element.select("img").attr("data-src");
-            String url = element.select("a").attr("href");
-            String name = element.select("div.detail > h6").text();
-            String id = url.split("/")[4];
-            list.add(new Vod(id, name, pic));
-        }
-        Integer total = (Integer.parseInt(pg)+1)*20;
-        return Result.string(Integer.parseInt(pg),Integer.parseInt(pg)+1,20,total,list);
+		if (tid == "all"){
+			String target = siteUrl "vod/show/id/all/page/" + Integer.parseInt(pg)+ "/";
+			Document doc = Jsoup.parse(OkHttp.string(target, getHeaders()));
+			for (Element element : doc.select("div.myui-vodlist__box")) {
+				String pic = element.select("a.myui-vodlist__thumb").attr("data-original");
+				String url = element.select("a.myui-vodlist__thumb").attr("href");
+				String name = element.select("a.myui-vodlist__thumb").attr("title");
+				String id = url.split("/")[4];
+				list.add(new Vod(id, name, pic));
+			}			
+		} else {
+			String target = cateUrl + tid + "/page/" + Integer.parseInt(pg)+ "/";
+			Document doc = Jsoup.parse(OkHttp.string(target, getHeaders()));
+			for (Element element : doc.select("div.myui-vodlist__box")) {
+				String pic = element.select("a.myui-vodlist__thumb").attr("data-original");
+				String url = element.select("a.myui-vodlist__thumb").attr("href");
+				String name = element.select("a.myui-vodlist__thumb").attr("title");
+				String id = url.split("/")[4];
+				list.add(new Vod(id, name, pic));
+			}
+		}
+        return Result.string(list);
     }
 
     @Override
     public String detailContent(List<String> ids) throws Exception {
         Document doc = Jsoup.parse(OkHttp.string(detailUrl.concat(ids.get(0)).concat("/"), getHeaders()));
-        String name = doc.select("meta[property=og:title]").attr("content");
-        String pic = doc.select("meta[property=og:image]").attr("content");
-        String year = doc.select("span.inactive-color").get(0).text();
+        String name = doc.select("h1.title]").text();
+        String pic = doc.select("img.lazyload").attr("data-original");
+		String url = doc.select("div.myui-content__operate").attr("href");
+		doc = Jsoup.parse(OkHttp.string(siteUrl.concat(url), getHeaders()));
+		String text = String.valueOf(doc);
+        String pattern = "'([^']+?\$https://vostrely\.com[^']+)";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(text);
+		String urlall = "";
+        while(matcher.find()) {
+            urlall.concat(matcher.group(1),"#");
+        }
+        url = urlall.substring(0, str.length() - 1);
         Vod vod = new Vod();
         vod.setVodId(ids.get(0));
         vod.setVodPic(pic);
-        vod.setVodYear(year.replace("上市於 ", ""));
         vod.setVodName(name);
-        vod.setVodPlayFrom("Jable");
-        vod.setVodPlayUrl("播放$" + Util.getVar(doc.html(), "hlsUrl"));
+        vod.setVodPlayFrom("JavBus");
+        vod.setVodPlayUrl(url);
         return Result.string(vod);
     }
 
