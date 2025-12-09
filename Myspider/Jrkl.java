7fbe9ca3a58b3539d4d328acf9100b5d52547b20
crarm <61692711+crarm@users.noreply.github.com>
2025-12-09 21:20:45 +0800

    Update Jrkl.java

--- Myspider/Jrkl.java
+++ Myspider/Jrkl.java
@@ -81,12 +81,9 @@ public class Jrkl extends Spider {
     @Override
     public String detailContent(List<String> ids) throws Exception {
         Document doc = Jsoup.parse(OkHttp.string(detailUrl.concat(ids.get(0)).concat("/"), getHeaders()));
-        String name = doc.select("h1.post-title").text();
+        String name = doc.select("h1.post-title ").text();
         String pic = siteUrl + doc.select("meta[itemprop=image]").attr("content");
         String html = doc.html();
-        // 打印 HTML 到控制台
-//        System.out.println(html);
-        // 2. 正则提取 window.$avdt 的 JSON 内容
         Pattern pattern = Pattern.compile("\"url\"\\s*:\\s*\"([^\"]+)\"");
         Matcher matcher =  pattern.matcher(html);
 
