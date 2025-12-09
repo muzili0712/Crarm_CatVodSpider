c5e32b01bb956f2405685e8cedb67f57bc0955c8
crarm <61692711+crarm@users.noreply.github.com>
2025-12-09 15:01:44 +0800

    Update Hlbdy.java

--- Myspider/Hlbdy.java
+++ Myspider/Hlbdy.java
@@ -23,10 +23,10 @@ import java.util.regex.Pattern;
 
 public class Hlbdy extends Spider {
 
-    private static final String siteUrl = "https://ikk.jiukun66.autos";
-    private static final String cateUrl = siteUrl + "/video/category/";
-    private static final String detailUrl = siteUrl + "/video/view/";
-    private static final String searchUrl = siteUrl + "/search?keywords=";
+    private static final String siteUrl = "https://across.odrepgn.cc";
+    private static final String cateUrl = siteUrl + "/category/";
+    private static final String detailUrl = siteUrl + "/archives/";
+    private static final String searchUrl = siteUrl + "/search/";
 
     private HashMap<String, String> getHeaders() {
         HashMap<String, String> headers = new HashMap<>();
@@ -37,18 +37,12 @@ public class Hlbdy extends Spider {
     private List<Vod> parseVods(Document doc) {
         List<Vod> list = new ArrayList<>();
         for (Element element : doc.select("article")) {
-            String pic = element.select("img").attr("src");
+            String pic = element.select("script").html();
             String url = element.select("a").attr("href");
-            String name = element.select("h4").text();
-            if (pic.endsWith(".gif") || name.isEmpty()) continue;
-//            if (!url.startsWith("http")) {
-//                pic = pic.replace("background-image: url('", "").replace("')", "");
-//                if (!pic.startsWith("http")) pic = "https:" + pic;
-//                String id = url.split("/")[3];
-//                list.add(new Vod(id, name, pic));
-//            }
-            String id = url.split("/")[3];
-            list.add(new Vod(id, name, pic));
+            String name = element.select("h2.post-card-title").text();
+            if (pic.contains(".gif") || name.isEmpty()) continue;
+            String id = url.split("/")[2].replace(".html","");
+            list.add(new Vod(id, name, "xxx"));
         }
         return list;
     }
@@ -56,27 +50,24 @@ public class Hlbdy extends Spider {
     @Override
     public String homeContent(boolean filter) throws Exception {
         List<Class> classes = new ArrayList<>();
-        String[] typeIdList = {"latest", "hd", "recent-favorite", "hot-list", "recent-rating", "nonpaid", "ori", "long-list", "longer-list", "month-discuss", "top-favorite", "most-favorite", "top-list", "top-last"};
-        String[] typeNameList = {"最近更新", "高清视频", "最近加精", "当前最热", "最近得分", "非付费", "91原创", "10分钟以上", "20分钟以上", "本月讨论", "本月收藏", "收藏最多", "本月最热", "上月最热"};
-        for (int i = 0; i < typeNameList.length; i++) {
-            classes.add(new Class(typeIdList[i], typeNameList[i]));
+		Document doc = Jsoup.parse(OkHttp.string(siteUrl+"/", getHeaders()));
+        for (Element element : doc.select("div.category-list > ul > li")) {
+            String typeId = element.select("a").attr("href").split("/")[2];
+            String typeName = element.select("a").text();
+            classes.add(new Class(typeId, typeName));
         }
-        Document doc = Jsoup.parse(OkHttp.string(siteUrl, getHeaders()));
         List<Vod> list = parseVods(doc);
         return Result.string(classes, list);
     }
 
     @Override
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) throws Exception {
-        String target = cateUrl + tid + "/" + pg;
-//        System.out.printf("target ===> " + target);
+        String target = cateUrl  + tid + "/";
+		if( !pg.equals("1")) target = cateUrl + tid + "/" + pg +"/";
         Document doc = Jsoup.parse(OkHttp.string(target, getHeaders()));
         List<Vod> list = parseVods(doc);
-//        System.out.println("=============================>>>>> ");
-//        System.out.println(list);
-//        System.out.println("=============================>>>>> ");
-        Integer total = (Integer.parseInt(pg) + 1) * 20;
-        return Result.string(Integer.parseInt(pg), Integer.parseInt(pg) + 1, 20, total, list);
+
+        return Result.string(list);
     }
 
     @Override
@@ -95,9 +86,7 @@ public class Hlbdy extends Spider {
         String playUrl = "";
         if (matcher.find()) {
             String json = matcher.group(1).replaceAll("\\\\/", "/");
-//            System.out.println("✅ 提取到 JSON: " + json); // ✅ 调试输出
 
-            // 3. 解析 JSON 并构造播放地址
             JSONObject avdt = new JSONObject(json);
             String hls = avdt.optString("hls");
             JSONArray cdns = avdt.optJSONArray("cdns");
@@ -115,7 +104,7 @@ public class Hlbdy extends Spider {
         vod.setVodPic(pic);
         vod.setVodYear(year);
         vod.setVodName(name);
-        vod.setVodPlayFrom("J91");
+        vod.setVodPlayFrom("Hlbdy");
         vod.setVodPlayUrl("播放$" + playUrl);
         return Result.string(vod);
     }
