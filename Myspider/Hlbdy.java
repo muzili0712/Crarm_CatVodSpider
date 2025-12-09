c7986c8850c8d62cbfc31187cf6e0524d56d41d0
crarm <61692711+crarm@users.noreply.github.com>
2025-12-09 13:08:45 +0800

    Update Hlbdy.java

--- Myspider/Hlbdy.java
+++ Myspider/Hlbdy.java
@@ -48,14 +48,14 @@ public class Hlbdy extends Spider {
             String picurl = element.select("script").html();
             String name = element.select("h2.post-card-title").text();
 			if (picurl.indexOf(".gif") >-1 || name.isEmpty()) continue;
-			Pattern pattern = Pattern.compile("loadBannerDirect\\s*\\(\\s*['\"]([^'\"]+)['\"]");
-            Matcher matcher = pattern.matcher(picurl);
-			picurl = matcher.find() ? matcher.group(1) : "";
-			ImageDecryptor imagedecryptor = new ImageDecryptor("","","","","","","");
-			String pic = imagedecryptor.downloadAndDecryptImage(picurl);
+			//Pattern pattern = Pattern.compile("loadBannerDirect\\s*\\(\\s*['\"]([^'\"]+)['\"]");
+            //Matcher matcher = pattern.matcher(picurl);
+			//picurl = matcher.find() ? matcher.group(1) : "";
+			//ImageDecryptor imagedecryptor = new ImageDecryptor("","","","","","","");
+			//String pic = imagedecryptor.downloadAndDecryptImage(picurl);
             String url = element.attr("href");
             String id = url.split("/")[2].replace(".html","");
-            list.add(new Vod(id, name, pic));
+            list.add(new Vod(id, name, picurl));
         }
         return Result.string(classes, list);
     }
