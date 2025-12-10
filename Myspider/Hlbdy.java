b9000897b7cf0e3428fe7363741ff819d9b6aa03
crarm <61692711+crarm@users.noreply.github.com>
2025-12-10 21:40:06 +0800

    Update Hlbdy.java

--- Myspider/Hlbdy.java
+++ Myspider/Hlbdy.java
@@ -181,7 +181,7 @@ public class Hlbdy extends Spider {
         vod.setVodPic(pic);
         vod.setVodYear(year);
         vod.setVodName(name);
-		vod.setContent("vodid="+ ids.get(0) +"--div.dplayer.video:" +json1.toString()+ "-----playUrl:"+playUrl);
+		vod.setVodContent("vodid="+ ids.get(0) +"--div.dplayer.video:" +json1.toString()+ "-----playUrl:"+playUrl);
         vod.setVodPlayFrom("Hlbdy");
         vod.setVodPlayUrl(playUrl);
         return Result.string(vod);
