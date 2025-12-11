ba476c2182c2834321dfef246a01a1fa23b192ff
crarm <61692711+crarm@users.noreply.github.com>
2025-12-11 14:13:50 +0800

    Update HEILIAO.java

--- Myspider/HEILIAO.java
+++ Myspider/HEILIAO.java
@@ -304,7 +304,7 @@ public class HEILIAO extends Spider {
         		}
     		}
     		return Result.string(list);
-		} catch (JSONException e) {
+		} catch (Exception  e) {
 			return Result.string(list);
 		}
     }
