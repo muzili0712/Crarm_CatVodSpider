8510b9c40b606c4a39fb07864ac71d75cd0a1e6e
crarm <61692711+crarm@users.noreply.github.com>
2025-12-17 13:11:37 +0800

    Update Cg51.java

--- Myspider/Cg51.java
+++ Myspider/Cg51.java
@@ -68,7 +68,7 @@ public class Cg51 extends Spider {
         List<ArticleData> dataList = new ArrayList<>();
         
         for (Element element : doc.select("article")) {
-            String url = element.select("a").attr("href").replace(siteUrl,"");
+            String url = element.select("a").attr("href").split("gfyxfdbt.cc")[1];
             String name = element.select(".post-card-title").text();
             
             if (url.isEmpty() || name.isEmpty()) {
