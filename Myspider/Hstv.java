b3b5d7bfce541235794bccf5b98b294e86173c8f
crarm <61692711+crarm@users.noreply.github.com>
2025-12-17 10:38:25 +0800

    Create Hstv.java

---
+++ Myspider/Hstv.java
@@ -0,0 +1,105 @@
+package com.github.catvod.spider;
+
+import com.github.catvod.bean.Class;
+import com.github.catvod.bean.Result;
+import com.github.catvod.bean.Vod;
+import com.github.catvod.crawler.Spider;
+import com.github.catvod.net.OkHttp;
+import com.github.catvod.utils.Util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import java.net.URL;
+import java.net.MalformedURLException;
+
+public class Hstv extends Spider {
+
+    private static final String siteUrl = "https://hsex.icu/";
+
+    private HashMap<String, String> getHeaders() {
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("User-Agent", Util.CHROME);
+        return headers;
+    }
+
+    private List<Vod> parseVods(Document doc) {
+        List<Vod> list = new ArrayList<>();
+        for (Element element : doc.select("div.thumbnail")) {
+            String pic = element.select("div.image").attr("style").split("'")[1];
+            String url = element.select("a").attr("href");
+            String name = element.select("a").attr("title");
+            String id = url.replace("video-", "").replace(".htm", "");
+            list.add(new Vod(id, name, pic));
+        }
+        return list;
+    }
+    
+    @Override
+    public String homeContent(boolean filter) throws Exception {
+        
+        List<Class> classes = new ArrayList<>();
+        Document doc = Jsoup.parse(OkHttp.string(siteUrl, getHeaders()));
+        for (Element element : doc.select("ul.nav.navbar-nav a")) {
+			      String typeId = element.attr("href").split("-")[0];
+			      String typeName = element.text();
+			      classes.add(new Class(typeId, typeName));
+        }
+        List<Vod> list = parseVods(doc);
+        return Result.string(classes, list);
+    }
+
+    @Override
+    public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) throws Exception {
+        String target = siteUrl.concat(tid).concat("-").concat(pg).concat(".html");
+        Document doc = Jsoup.parse(OkHttp.string(target, getHeaders()));
+        List<Vod> list = parseVods(doc);
+        return Result.string(list);
+    }
+
+    @Override
+    public String detailContent(List<String> ids) throws Exception {
+        String html = OkHttp.string(siteUrl.concat("video-").concat(ids.get(0)).concat(".htm"), getHeaders());
+		    Document doc = Jsoup.parse(html);
+        String url = doc.select("source.video-source").attr("src");
+        String name = doc.select("h3.panel-title").text();
+        String pic = doc.select("video").attr("poster");
+		
+        Vod vod = new Vod();
+        vod.setVodId(ids.get(0));
+        vod.setVodPic(pic);
+        vod.setVodName(name);
+        vod.setVodPlayFrom("Hstv");
+        vod.setVodPlayUrl("播放$"+url);
+		    return Result.string(vod);
+    }
+
+    @Override
+    public String searchContent(String key, boolean quick) throws Exception {
+        return searchContent(key, "1");
+    }
+
+    @Override
+    public String searchContent(String key, boolean quick, String pg) throws Exception {
+        return searchContent(key, pg);
+    }
+	
+    private String searchContent(String key, String pg) {
+        String target = siteUrl.concat("page/").concat(pg).concat("?s=").concat(key);
+        Document doc = Jsoup.parse(OkHttp.string(target, getHeaders()));
+        List<Vod> list = parseVods(doc);
+        return Result.string(list);
+    }
+
+    @Override
+    public String playerContent(String flag, String id, List<String> vipFlags) throws Exception {
+        return Result.get().url(id).header(getHeaders()).string();
+    }
+}
